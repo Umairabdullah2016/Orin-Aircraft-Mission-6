899bc825a1d24dcdf8bfb1b6ac4d6d27d24f40dd
Umairabdullah2016 <ua8839705@gmail.com>
2026-03-28 13:15:48 +0000

    Refactor HTML and CSS for game container

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,37 +5,30 @@ st.set_page_config(layout="wide")
 
 html_code = """
 <style>
-#wrap {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 95vh;
-    background: #000;
+body {
+    margin: 0;
+    background: black;
 }
 
-#game {
+#game-container {
     width: 100%;
     height: 100%;
-    max-width: 900px;   /* keeps aspect ratio nice */
 }
 
-#game iframe {
+#game-container iframe {
     width: 100%;
-    height: 100%;
+    height: 1100px;  /* KEY FIX */
     border: none;
 }
 </style>
 
-<div id="wrap">
-    <div id="game">
-        <iframe 
-            src="https://arcade.makecode.com/---run?id=S87889-51924-82524-20479"
-            allowfullscreen
-            sandbox="allow-popups allow-forms allow-scripts allow-same-origin">
-        </iframe>
-    </div>
+<div id="game-container">
+    <iframe 
+        src="https://arcade.makecode.com/---run?id=S87889-51924-82524-20479"
+        allowfullscreen
+        sandbox="allow-popups allow-forms allow-scripts allow-same-origin">
+    </iframe>
 </div>
 """
 
-components.html(html_code, height=900)
+components.html(html_code, height=1100)
