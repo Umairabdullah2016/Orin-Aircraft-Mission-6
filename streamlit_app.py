6a804f78bcf52d41047f0bb31ed36f01627ed1a5
Umairabdullah2016 <ua8839705@gmail.com>
2026-03-28 13:12:56 +0000

    Adjust iframe height and add CSS styles for game
    Updated the iframe height in the HTML code and added CSS styles for the game container.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,31 @@ import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 
 html_code = """
-<div style="position:relative;height:2;padding-bottom:117.6%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:200%;" src="https://arcade.makecode.com/---run?id=S87889-51924-82524-20479" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-forms allow-scripts allow-same-origin" frameborder="0"></iframe></div>
+<style>
+#game-container {
+    position: relative;
+    width: 100%;
+    height: 85vh;   /* enough space for controls */
+    background: black;
+}
+
+#game-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;   /* critical: NOT 200% */
+    border: none;
+}
+</style>
+
+<div id="game-container">
+    <iframe 
+        src="https://arcade.makecode.com/---run?id=S87889-51924-82524-20479"
+        allowfullscreen
+        sandbox="allow-popups allow-forms allow-scripts allow-same-origin">
+    </iframe>
+</div>
 """
 
-components.html(html_code, height=700)
+components.html(html_code, height=750)
