0c35525dfdce70b2224750c3940de24824f9f662
Umairabdullah2016 <ua8839705@gmail.com>
2026-03-28 13:10:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,31 +4,7 @@ import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 
 html_code = """
-<style>
-#game-container {
-    position: relative;
-    width: 100%;
-    height: 80vh;  /* KEY FIX */
-    background: black;
-}
-
-#game-container iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border: none;
-}
-</style>
-
-<div id="game-container">
-    <iframe 
-        src="https://arcade.makecode.com/---run?id=S00850-85347-54510-79145"
-        allowfullscreen
-        sandbox="allow-popups allow-forms allow-scripts allow-same-origin">
-    </iframe>
-</div>
+<div style="position:relative;height:0;padding-bottom:117.6%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="https://arcade.makecode.com/---run?id=S87889-51924-82524-20479" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-forms allow-scripts allow-same-origin" frameborder="0"></iframe></div>
 """
 
 components.html(html_code, height=700)
