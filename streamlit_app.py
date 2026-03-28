9c0b774fd0e70bf1088440a24569983943b5e944
Umairabdullah2016 <ua8839705@gmail.com>
2026-03-28 13:11:57 +0000

    Update iframe HTML code in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 
 html_code = """
-<div style="position:relative;height:0;padding-bottom:117.6%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:200%;" src="https://arcade.makecode.com/---run?id=S87889-51924-82524-20479" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-forms allow-scripts allow-same-origin" frameborder="0"></iframe></div>
+<div style="position:relative;height:2;padding-bottom:117.6%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:200%;" src="https://arcade.makecode.com/---run?id=S87889-51924-82524-20479" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-forms allow-scripts allow-same-origin" frameborder="0"></iframe></div>
 """
 
 components.html(html_code, height=700)
